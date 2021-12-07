114507477b9f6707c221df70d2678c836d1435c0
ednew2018 <shenjian8628@gmail.com>
2021-12-07 14:03:30 +0800

    Add emoji react app

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
-import React from './react'
-import ReactDOM from './react-dom'
-
-class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            num: 0
-        }
-    }
-
-    componentWillUpdate() {
-        console.log('update');
-    }
-
-    componentWillMount() {
-        console.log('mount');
-    }
-
-    onClick() {
-        for (let i = 0; i < 100; i++) {
-            this.setState({num: this.state.num + 1});
-        }
-    }
-
-    render() {
-        return (
-            <div onClick={() => this.onClick()}>
-                <h1>number: {this.state.num}</h1>
-                <button>add</button>
-            </div>
-        );
-    }
-}
+import React from "../react/react";
+import ReactDOM from "../react/react-dom";
+import App from "./emoji/App";
+import "./emoji/index.css";
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
 ReactDOM.render(
-    <Counter />,
-    document.getElementById('root')
+	<App />, 
+	document.getElementById("root")
 );

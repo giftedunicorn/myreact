167e61e4f469e4dcf38d93437b2c386a24579501
ednew2018 <shenjian8628@gmail.com>
2021-12-15 18:04:53 +0800

    Add rules and fix webpack compile

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
-  output:{
+  output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["css-loader"],
+      },
+    ]
   }
 }
-
